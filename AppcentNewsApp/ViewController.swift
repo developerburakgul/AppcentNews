9945ff6af166e18a8b78c69029f5fb373532e4ee
Burak GÜL <developerburakgul@gmail.com>
2025-01-10 21:17:24 +0300

    Initial Commit

---
+++ AppcentNewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppcentNewsApp
+//
+//  Created by Burak Gül on 10.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
